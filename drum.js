0c8858ee2f7a1585c82a0ae020e2b50eee155c99
Suyoscar11 <suyoscar11@gmail.com>
2023-03-27 14:32:57 +0545

    test case 1: for loops and while loops

--- drum.js
+++ drum.js
@@ -1,5 +1,7 @@
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick() {
-  alert("Drum bajauni ho?");
+var t = document.querySelectorAll("button").length;
+while (t--) {
+  document.querySelectorAll("button")[t].addEventListener("click", handleClick);
+  function handleClick() {
+    alert("Drum bajauni ho?");
+  }
 }
