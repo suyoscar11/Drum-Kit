bf41f6d9ce18b2c9a2419ffbfafdaca6c95ec632
Suyoscar11 <suyoscar11@gmail.com>
2023-03-27 18:06:44 +0545

    js sucks

--- drum.js
+++ drum.js
@@ -1,8 +1,40 @@
-var t = document.querySelectorAll("button").length;
-while (t--) {
-  document.querySelectorAll("button")[t].addEventListener("click", handleClick);
-  function handleClick() {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-  }
+var t = document.querySelectorAll(".drum").length;
+for (var i = 0; i <= t; i++) {
+  document.querySelectorAll(".drum")[t].addEventListener("click", function () {
+    var innerhtml = this.innerHTML;
+    switch (innerhtml) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var tom5 = new Audio("sounds/kick-bass.mp3");
+        tom5.play();
+        break;
+
+      case "k":
+        var audi = new Audio("sounds/snare.mp3");
+        audi.play();
+        break;
+
+      case "l":
+        var audioo = new Audio("sounds/crash.mp3");
+        audioo.play();
+        break;
+      default:
+        console.log(innerhtml);
+    }
+  });
 }
