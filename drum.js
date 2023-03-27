6d2bcf9265bdc87c9122a3dbe6d779fca13d56e0
Suyoscar11 <suyoscar11@gmail.com>
2023-03-27 15:15:29 +0545

    added bg images to buttons&& added one sound

--- drum.js
+++ drum.js
@@ -2,6 +2,7 @@ var t = document.querySelectorAll("button").length;
 while (t--) {
   document.querySelectorAll("button")[t].addEventListener("click", handleClick);
   function handleClick() {
-    alert("Drum bajauni ho?");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
   }
 }
